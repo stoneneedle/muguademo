f8fa5c53362d3ab571a2d5b27a32f610eee7ca66
stoneneedle <stoneneedle@gmail.com>
2022-06-19 16:44:07 -0400

    Added test DB objects via shell, created admin superuser.

--- mugua/main/views.py
+++ mugua/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Jimbo's Site</h1>")
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br /><p>%s</p>" %(ls.name, item.text))
